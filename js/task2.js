0767cfd2d56a565f2456d452f53700d17e49495b
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-08 14:14:18 +0300

    Remove all js

--- js/task2.js
+++
@@ -1,6 +0,0 @@
-var user1 = {name: "Vasya", age: 25},
-user2 = {name: "Masha", age: 18 },
-user3 = {name: "Olyaksandr", age: 56 }; 
-var userarray = [user1,user2,user3];
-console.log(userarray);
-userarray.sort(function (a, b){return a.age - b.age});
--- js/task3.js
+++
@@ -1,6 +0,0 @@
-var heroes = ["Iron Man", "Deadpool", "Hulk", "Doctor Strange"];
-console.log(heroes);
-var heroid = heroes.indexOf('Hulk');
-heroes.splice (heroid,1);
-console.log(heroes);
-document.write(heroes);
