585da1c714ae08aae950598ceef9c8c0dff92beb
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-08 14:21:44 +0300

    Add task 3

---
+++ js/Task_3.js
@@ -0,0 +1,7 @@
+// Задание 3
+var heroes = ["Iron Man", "Deadpool", "Hulk", "Doctor Strange"];
+console.log(heroes);
+var heroid = heroes.indexOf('Hulk');
+heroes.splice (heroid,1);
+console.log(heroes);
+document.write(heroes);
