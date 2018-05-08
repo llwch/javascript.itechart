4a6983ba198258851a928d1af205c8aa35295f1e
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-08 18:18:29 +0300

    Add task 2

---
+++ js/task_2.js
@@ -0,0 +1,6 @@
+// Задание 2
+var user1 = {name: "Vasya", age: 25},
+user2 = {name: "Masha", age: 18 },
+user3 = {name: "Olyaksandr", age: 56 }; 
+var userarray = [user1,user2,user3];
+console.log(userarray.sort(function (a, b){return a.age - b.age}));
