40c2eb4de940980409a58980cf79683209b5e93c
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-10 14:09:02 +0300

    Make it beautiful again

--- js/task_2.js
+++ js/task_2.js
@@ -1,6 +1,17 @@
 // Задание 2
-var user1 = {name: "Vasya", age: 25},
-user2 = {name: "Masha", age: 18 },
-user3 = {name: "Olyaksandr", age: 56 }; 
-var userArray = [user1,user2,user3];
-console.log(userArray.sort(function (a, b){return a.age - b.age}));
+var user1 = {
+    name: "Vasya",
+    age: 25
+},
+user2 = {
+    name: "Masha",
+    age: 18
+},
+user3 = {
+    name: "Olyaksandr",
+    age: 56
+};
+var userArray = [user1, user2, user3];
+console.log(userArray.sort(function(a, b) {
+return a.age - b.age
+}));
