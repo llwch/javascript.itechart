791b1979b3f1805c60c61b4314b8068de2430dfb
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-10 14:07:58 +0300

    Make it beautiful again

--- js/task_1.js
+++ js/task_1.js
@@ -3,14 +3,14 @@ var salaries = {
     "Vasya": 4500,
     "Masha": 2000,
     "Olyaksandr": 2700
-    };
-    var maxSalary = 0;
-    var nameWithMaxSalary = null;
-    for (var name in salaries) {
-        if (maxSalary < salaries[name]) {
-            maxSalary = salaries[name];
-            nameWithMaxSalary = name;
-        }
-      }
-    console.log (nameWithMaxSalary || "нет сотрудников" );
-    document.write (nameWithMaxSalary || "нет сотрудников" );
+};
+var maxSalary = 0;
+var nameWithMaxSalary = null;
+for (var name in salaries) {
+    if (maxSalary < salaries[name]) {
+        maxSalary = salaries[name];
+        nameWithMaxSalary = name;
+    }
+}
+console.log(nameWithMaxSalary || "нет сотрудников");
+document.write(nameWithMaxSalary || "нет сотрудников");
