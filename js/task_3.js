9c9316de96ad4f34d2b883de7a16a2b45cffddb6
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-10 14:10:04 +0300

    Make it beautiful again

--- js/task_3.js
+++ js/task_3.js
@@ -2,6 +2,6 @@
 var heroes = ["Iron Man", "Deadpool", "Hulk", "Doctor Strange"];
 console.log(heroes);
 var heroId = heroes.indexOf('Hulk');
-heroes.splice (heroId,1);
+heroes.splice(heroId, 1);
 console.log(heroes);
 document.write(heroes);
