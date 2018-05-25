17b332115fddcd8a2d8d5e0142189f438c279673
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-05-25 19:08:50 +0300

    Add server

---
+++ website/js/arrayList.js
@@ -0,0 +1,4 @@
+var mercedesModels = ["Mercedes 190", "Mercedes A-class", "Mercedes CLA", "Mercedes SLK", "Mercedes E-class", "Mercedes S-class"];
+var toyotaModels = ["Avensis", "Camry", "Corolla", "Highlander", "Land Cruiser", "RAV 4"];
+var ladaModels = ["XRAY", "Vesta VEDRO", "Kalina", "Vesta Cross", "Granta", "Priora"];
+var manufacturerList = ["Mercedes", "Toyota", "Lada", ""];
