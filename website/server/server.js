22debf1db04e7fba63ee8f5ff5eb81b45c353482
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-06-09 22:51:53 +0300

    update server

--- website/server/server.js
+++ website/server/server.js
@@ -1,11 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 8080;
 const server = http.createServer();
+var showSite = fs.readFileSync('/Users/tibraid/Documents/javascript.itechart/website/index.html');
 
 server.on('request', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('dummy');
+  res.end(showSite);
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
