28a16a23cba4715b87c307db94848d443b194c6f
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-06-09 22:38:04 +0300

    Add dummy server

---
+++ website/server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('dummy');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
